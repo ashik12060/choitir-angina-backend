d12ed3afeddcd9000d74d82eb629b54b2924c8c1
Ashik Mahmood <wd.ashik04@gmail.com>
2025-09-02 19:00:57 +0600

    shop assigner update pertially

--- backend/models/shopModel.js
+++ backend/models/shopModel.js
@@ -1,23 +1,6 @@
 const mongoose = require("mongoose");
 const Variant = require("../models/variantModel"); // Import the Variant model
 
-// const shopSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   location: { type: String },
-//   products: [
-//     {
-//       product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-//       variants: [
-//         {
-//           variant: { type: mongoose.Schema.Types.ObjectId, ref: "Variant", required: true },
-//           assignedQuantity: { type: Number, required: true, default: 0 },
-//         }
-//       ]
-//     }
-//   ],
-//   createdAt: { type: Date, default: Date.now },
-// });
-
 const shopSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String },
